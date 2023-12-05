d713e6a80ef78a6fab8fc23cdf5f9b952f413c3a
Yahir <yah.rivera>
2023-12-05 16:52:41 -0600

    Initial Commit

---
+++ workshop/workshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  workshopApp.swift
+//  workshop
+//
+//  Created by gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct workshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
